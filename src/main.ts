86d24108c53723e03673ff2d5e2bc657cf6cc081
Rohringer Zoltan <rohringer.zoltan@gmail.com>
2023-05-15 12:58:53 +0200

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Project management api')
+    .setDescription('API for user project and task management')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
